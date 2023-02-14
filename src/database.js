9d4a9abddef98e96f69a27dc1a6af1d7a40b6044
Vitor Guilhermo <viguilhermo@hotmail.com>
2023-02-13 21:05:03 -0300

    separete routes

--- src/database.js
+++ src/database.js
@@ -1,12 +1,12 @@
 import fs from 'node:fs/promises';
 
-const databasePath = new URL('../db.json', import.meta.url)
+const databasePath = new URL('../db.json', import.meta.url);
 
 export class Database {
     #database = {}
 
     constructor() {
-        fs.readFile(databasePath, 'utf-8')
+        fs.readFile(databasePath, 'utf8')
             .then(data => {
                 this.#database = JSON.parse(data)
             })
@@ -16,7 +16,7 @@ export class Database {
     }
 
     #persist() {
-        fs.writeFile(databasePath, JSON.stringify(this.#database))
+        fs.writeFile(databasePath, JSON.stringify(this.#database));
     }
 
     insert(table, data) {
