90f16bf5ee1d3f65f4332a3d0c7b179968299698
Vitor Guilhermo <viguilhermo@hotmail.com>
2023-02-14 22:48:55 -0300

    add update, delete and filter users

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,20 @@
 import { randomUUID } from "node:crypto";
 import { Database } from "./database.js";
+import { buildRoutePath } from "./utils/build-route-path.js";
 
 const database = new Database();
 
 export const routes = [
     {
         method: 'GET',
-        path: '/users',
+        path: buildRoutePath('/users'),
         handler: (req, res) => {
-            const users = database.select('users');
+            const { search } = req.query
+            
+            const users = database.select('users', {
+                name: search,
+                email: search,
+            });
 
             return res
                 .end(JSON.stringify(users));
@@ -16,7 +22,7 @@ export const routes = [
     },
     {
         method: 'POST',
-        path: '/users',
+        path: buildRoutePath('/users'),
         handler: (req, res) => {
             const { name, email } = req.body;
 
@@ -29,6 +35,29 @@ export const routes = [
             database.insert('users', user);
 
             return res.writeHead(201).end();
+        }
+    },
+    {
+        method: 'PUT',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const { id } = req.params
+            const { name, email } = req.body
+
+            database.update('users', id, { name, email })
+
+            return res.writeHead(204).end()
+        }
+    },
+    {
+        method: 'DELETE',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const { id } = req.params
+
+            database.delete('users', id)
+
+            return res.writeHead(204).end()
         }
     }
 ]
